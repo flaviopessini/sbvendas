157bd879a109949dfcad08e76d1c2761056e3052
Flávio Henrique Pessini <flavio.pessini@outlook.com>
2022-11-21 22:00:23 -0300

    Initial commit

---
+++ sbvendas/src/test/java/com/flaviopessini/sbvendas/SbvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flaviopessini.sbvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
