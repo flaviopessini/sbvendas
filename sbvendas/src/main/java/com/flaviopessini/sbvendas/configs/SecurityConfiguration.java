dc1086a67719b135d8ad3e4b6df0d210c1f894ee
flaviohp <flaviohenrique@lorensid.com>
2022-11-29 10:24:44 -0300

    implementação roles de acesso

--- sbvendas/src/main/java/com/flaviopessini/sbvendas/configs/SecurityConfiguration.java
+++ sbvendas/src/main/java/com/flaviopessini/sbvendas/configs/SecurityConfiguration.java
@@ -25,9 +25,11 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests()
-                .antMatchers("/api/clientes/**").authenticated()
+                .antMatchers("/api/clientes/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/api/pedidos/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/api/produtos/**").hasRole("ADMIN")
                 .and()
-                .formLogin().permitAll();
+                .httpBasic();
 
         return http.build();
     }
