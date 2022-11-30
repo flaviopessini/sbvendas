da153b9bc7a2b5ed622559945e759565984464fb
flaviohp <flaviohenrique@lorensid.com>
2022-11-30 10:20:01 -0300

    implementação autenticação com Beans

--- sbvendas/src/main/java/com/flaviopessini/sbvendas/configs/SecurityConfiguration.java
+++ sbvendas/src/main/java/com/flaviopessini/sbvendas/configs/SecurityConfiguration.java
@@ -1,12 +1,19 @@
 package com.flaviopessini.sbvendas.configs;
 
+import com.flaviopessini.sbvendas.services.impl.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -14,11 +21,42 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration {
+
+    @Autowired
+    @Lazy
+    private UsuarioServiceImpl usuarioService;
+
+    @Autowired
+    @Lazy
+    private UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    @Lazy
+    public UserDetailsService userDetailsService() {
+        return new UsuarioServiceImpl();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
     @Bean
@@ -29,19 +67,15 @@ public class SecurityConfiguration {
                 .antMatchers("/api/pedidos/**").hasAnyRole("ADMIN", "USER")
                 .antMatchers("/api/produtos/**").hasRole("ADMIN")
                 .and()
+                .authenticationProvider(this.authenticationProvider())
                 .httpBasic();
 
         return http.build();
     }
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers("/ignore1", "/ignore2");
-    }
-
-    @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
-        final UserDetails user = User.builder()
+        /*final UserDetails user = User.builder()
                 .username("user")
                 .password(passwordEncoder().encode("user"))
                 .roles("USER")
@@ -50,8 +84,8 @@ public class SecurityConfiguration {
                 .username("admin")
                 .password(passwordEncoder().encode("admin"))
                 .roles("USER", "ADMIN")
-                .build();
-        System.out.println(user.getPassword());
-        return new InMemoryUserDetailsManager(user, admin);
+                .build();*/
+        final var admin = this.usuarioService.loadUserByUsername("admin");
+        return new InMemoryUserDetailsManager(admin);
     }
 }
