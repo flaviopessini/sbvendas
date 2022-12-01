daa6d8bf5c75dac011d7d58de3c798d36ffa5852
flaviohp <flaviohenrique@lorensid.com>
2022-12-01 15:48:26 -0300

    Ajusta para gerar um package .war

--- sbvendas/src/main/java/com/flaviopessini/sbvendas/SbvendasApplication.java
+++ sbvendas/src/main/java/com/flaviopessini/sbvendas/SbvendasApplication.java
@@ -8,10 +8,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SbvendasApplication {
+public class SbvendasApplication extends SpringBootServletInitializer {
 
     /**
      * Obtém a configuração externalizada através do arquivo 'application.properties'.
